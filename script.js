6929dbc53238af7dae1a13391c480379c06fc5ba
Joel Ramos <joelramos456@gmail.com>
2024-05-27 14:23:59 -0500

    aplicar descuentos

--- script.js
+++ script.js
@@ -1,10 +1,86 @@
-function calcDesc(cant) {
-  let cant = document.getElementById("input-cant").value;
-  if (cant < 10) {
-    document.getElementById("desc-mono").innerHTML = cant * 3.5 + " %";
-  } else if (cant >= 10 && cant <= 20) {
-    document.getElementById("desc-mono").innerHTML = cant * 7.0 + " %";
-  } else if (cant > 20) {
-    document.getElementById("desc-mono").innerHTML = cant * 9.5 + " %";
+function calcDesc(toy, cant, prec) {
+  //let cant = document.getElementById("input-cant").value;
+  //let prec = document.getElementById("precio").value;
+
+  switch (toy) {
+    case "monopolio":
+      if (cant < 10) {
+        let desc = (prec * 3.5) / 100;
+        document.getElementById("desc-mono").innerHTML = "S/" + desc.toFixed(2);
+      } else if (cant >= 10 && cant <= 20) {
+        let desc = (prec * 7.0) / 100;
+        document.getElementById("desc-mono").innerHTML = "S/" + desc.toFixed(2);
+      } else if (cant > 20) {
+        let desc = (prec * 9.5) / 100;
+        document.getElementById("desc-mono").innerHTML = "S/" + desc.toFixed(2);
+      }
+      break;
+
+    case "ajedrez":
+      if (cant < 10) {
+        let desc = (prec * 3.5) / 100;
+        document.getElementById("desc-ajedrez").innerHTML =
+          "S/" + desc.toFixed(2);
+      } else if (cant >= 10 && cant <= 20) {
+        let desc = (prec * 7.0) / 100;
+        document.getElementById("desc-ajedrez").innerHTML =
+          "S/" + desc.toFixed(2);
+      } else if (cant > 20) {
+        let desc = (prec * 9.5) / 100;
+        document.getElementById("desc-ajedrez").innerHTML =
+          "S/" + desc.toFixed(2);
+      }
+      break;
+
+    case "lego":
+      if (cant < 10) {
+        let desc = (prec * 3.5) / 100;
+        document.getElementById("desc-lego").innerHTML =
+          "S/" + desc.toFixed(2);
+      } else if (cant >= 10 && cant <= 20) {
+        let desc = (prec * 7.0) / 100;
+        document.getElementById("desc-lego").innerHTML =
+          "S/" + desc.toFixed(2);
+      } else if (cant > 20) {
+        let desc = (prec * 9.5) / 100;
+        document.getElementById("desc-lego").innerHTML =
+          "S/" + desc.toFixed(2);
+      }
+      break;
+
+    case "dama":
+      if (cant < 10) {
+        let desc = (prec * 3.5) / 100;
+        document.getElementById("desc-dama").innerHTML =
+          "S/" + desc.toFixed(2);
+      } else if (cant >= 10 && cant <= 20) {
+        let desc = (prec * 7.0) / 100;
+        document.getElementById("desc-dama").innerHTML =
+          "S/" + desc.toFixed(2);
+      } else if (cant > 20) {
+        let desc = (prec * 9.5) / 100;
+        document.getElementById("desc-dama").innerHTML =
+          "S/" + desc.toFixed(2);
+      }
+      break;
+
+    case "laberinto":
+      if (cant < 10) {
+        let desc = (prec * 3.5) / 100;
+        document.getElementById("desc-laberinto").innerHTML =
+          "S/" + desc.toFixed(2);
+      } else if (cant >= 10 && cant <= 20) {
+        let desc = (prec * 7.0) / 100;
+        document.getElementById("desc-laberinto").innerHTML =
+          "S/" + desc.toFixed(2);
+      } else if (cant > 20) {
+        let desc = (prec * 9.5) / 100;
+        document.getElementById("desc-laberinto").innerHTML =
+          "S/" + desc.toFixed(2);
+      }
+      break;
+
+    default:
+      break;
   }
 }
